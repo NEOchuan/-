ab040cbd6aac201a7b78aa979ba1389ecb74c29e
NEOchuan <neochuan01@gmail.com>
2024-12-19 13:20:31 +0800

    修改了输出文件的逻辑，现在可以指定输出到指定目录当中了

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from datetime import datetime
 from extract_urls import extract_filtered_links
 from captcha_handler import recognize_captcha
 from extract_details import extract_target_data
+import os
 
 if __name__ == "__main__":
     # 解析命令行参数
     parser = argparse.ArgumentParser(description="通过关键词提取租房链接并处理详情页数据")
     parser.add_argument("-k", "--keyword", required=True, help="搜索关键词")
+    parser.add_argument("-o", "--output", required=True, help="输出CSV文件的目录")
     args = parser.parse_args()
 
     search_keyword = args.keyword  # 从命令行获取搜索关键词
+    output_dir = args.output  # 指定的输出目录
+
+    # 确保输出目录存在
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        print(f"[+] 创建输出目录: {output_dir}")
 
     # 使用关键词和当前日期生成输出文件名
     current_date = datetime.now().strftime("%Y%m%d")
-    output_csv = f"{search_keyword}_{current_date}.csv"
+    output_csv = os.path.join(output_dir, f"{search_keyword}_{current_date}.csv")
 
     # 提取URL
     print("[*] 开始提取URL...")
